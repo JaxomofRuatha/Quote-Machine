02001e24b08df5072a95f77b129445dbeb617646
JaxomofRuatha <jchritton33@gmail.com>
2017-09-28 23:06:58 -0500

    Updated apiSkeleton, still attempting to update theme

--- src/App.js
+++ src/App.js
@@ -27,8 +27,7 @@ export class App extends React.Component {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded"
       },
-      method: "GET",
-      mode: "cors"
+      method: "GET"
     }
 
     const url = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&json=?";
@@ -50,7 +49,7 @@ export class App extends React.Component {
       currentAuthor: res.quoteAuthor,
       theme
     }, () => {
-      document.html.style.backgroundColor = this.state.theme.colorbg;
+      document.body.style.backgroundColor = this.state.theme.colorbg;
     });
   }
 
--- src/utils/APIHelpers.js
+++ src/utils/APIHelpers.js
@@ -1,24 +1,62 @@
-export function checkStatus(response) {
-    if (!response.ok) {
-        const error = new Error(response.statusText || response.status);
-        error.response = response;
+export const REQUEST_TIMEOUT_MS = 30000;
+
+const _checkStatus = (res) => {
+    if (res.ok) {
+        return res[res.status === 204 ? "text" : "json"]();
+    } else if (res.status === 401) {
+        throw new Error('Unauthorized');
+    } else {
+        const error = new Error(res.statusText);
+        error.response = res;
         throw error;
     }
-    return response;
+}
+
+export function timedRequest(ms, promise) {
+    return new Promise((resolve, reject) => {
+        const timeoutId = setTimeout(() => {
+            reject(new Error('Request Timeout'));
+        }, ms);
+        promise.then(
+            (res) => {
+                clearTimeout(timeoutId);
+                resolve(res);
+            },
+            (err) => {
+                clearTimeout(timeoutId);
+                reject(err);
+            }
+        );
+    });
 }
 
 export function apiSkeleton(url, options, onRequestSuccess, onRequestFail) {
+    if (!url) onRequestFail(new Error('Request url is a required field'));
+    if (!options) onRequestFail(new Error('Request options is a required field'));
+    const reqOptions = {
+        mode: 'cors',
+        ...options
+    };
+    timedRequest(REQUEST_TIMEOUT_MS, fetch(url, reqOptions))
+        .then(_checkStatus)
+        .then((res) => {
+            onRequestSuccess(res);
+        })
+        .catch((error) => {
+            console.warn(error);
+            onRequestFail(error);
+        });
+}
+/*
+export function apiSkeleton(url, options, onRequestSuccess, onRequestFail) {
     fetch(url, options)
-        .then(checkStatus).catch((error) => {
+        .then(_checkStatus).catch((error) => {
             throw error;
         })
-
         .then((response) => response.json())
-
         .then((json) => {
             onRequestSuccess(json);
         })
-
         .catch((error) => {
             const response = error.response;
             if (response === undefined) {
@@ -27,7 +65,6 @@ export function apiSkeleton(url, options, onRequestSuccess, onRequestFail) {
                 error.status = response.status;
                 error.statusText = response.statusText;
                 response.text()
-
                     .then((text) => {
                         try {
                             const json = JSON.parse(text);
@@ -39,4 +76,6 @@ export function apiSkeleton(url, options, onRequestSuccess, onRequestFail) {
                     });
             }
         });
-}
+
+    }
+*/
