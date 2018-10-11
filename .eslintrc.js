018ebd1a4b62354916d903763fbf9c358bbdfa6f
John Chritton <jchritton33@gmail.com>
2018-10-11 13:52:19 -0500

    Updating ESLint and dependencies, disabling CRA preflight for Airbnb config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,22 +8,33 @@ module.exports = {
   },
   plugins: ['react', 'import'],
   rules: {
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.js', '.jsx']
-      }
-    ],
     'comma-dangle': [2, 'only-multiline'],
-    'quote-props': [1, 'consistent'],
-    'no-underscore-dangle': [0],
-    'no-plusplus': [0],
-    'no-use-before-define': [0],
-    'no-var': [1],
-    'no-undef': [2],
+    'import/no-extraneous-dependencies': [2, {
+      devDependencies: true
+    }],
+    'max-len': [1, {
+      ignoreUrls: true
+    }],
     'no-param-reassign': [1],
-    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
-    'react/jsx-no-bind': 1,
-    'no-unused-expressions': [1, { allowTaggedTemplates: true }]
+    'no-plusplus': [0],
+    'no-undef': [2],
+    'no-underscore-dangle': [0],
+    'no-unused-expressions': [1, {
+      allowTaggedTemplates: true,
+      allowTernary: true
+    }],
+    'no-use-before-define': [0],
+    'quote-props': [1, 'consistent'],
+
+    /*
+    *  React plugin rules
+    */
+    'react/destructuring-assignment': [2, {
+      ignoreClassFields: true
+    }],
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.js', '.jsx']
+    }],
+    'react/jsx-no-bind': 1
   }
 };
